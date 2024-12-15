abefce0a56c80bbd9b11fbff8bf04a84431e3e4f
Melvin5808 <127872703+Melvin5808@users.noreply.github.com>
2024-12-15 12:42:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve index.html at the root (/)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
